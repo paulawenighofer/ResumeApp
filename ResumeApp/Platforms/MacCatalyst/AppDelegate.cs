c9283682739691a86156e9bd830ed73958a87e34
paulawenighofer <125960357+paulawenighofer@users.noreply.github.com>
2026-03-14 22:49:55 +0100

    setup

---
+++ ResumeApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ResumeApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
